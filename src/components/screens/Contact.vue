110c5c08ad97b03f3c41b261ca99fa8db0dae003
hkm007 <hkm0072001@gmail.com>
2020-11-11 22:34:25 +0530

    other screens

--- src/components/screens/Contact.vue
+++ src/components/screens/Contact.vue
@@ -1,7 +1,22 @@
 <template>
     <div>
         <Navbar></Navbar>
-        <h1>Contact</h1>
+        <div class="container col-lg-8 mt-5 mb-4">
+            <h2>Contact Us</h2>
+            <hr style="border: 1px solid;" />
+            <form>
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">Email</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" autofocus>
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlTextarea1">Message</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
+                </div>
+                <button type="submit" class="btn btn-success btn-block">Submit</button>
+                <hr style="border: 1px solid;" />
+            </form>
+        </div>
         <Footer></Footer>
     </div>
 </template>
--- src/components/screens/Login.vue
+++ src/components/screens/Login.vue
@@ -1,23 +1,45 @@
 <template>
     <div>
         <Navbar></Navbar>
-        <h1>Login</h1>
+        <div class="container">
+            <div class="card mt-5 mb-4 col-lg-4 mx-auto">
+                <div class="card-body">
+                    <center><h2 class="card-title">Login</h2>
+                    <h6 class="card-subtitle mb-2">Admin Panel</h6></center>
+                    <hr style="border: 1px solid;" />
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1"><b>Email</b></label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email" autofocus>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1"><b>Password</b></label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <button type="submit" class="btn btn-danger btn-block">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
         <Footer></Footer>
     </div>
 </template>
 
 <script>
-import Navbar from '../Navbar';
 import Footer from '../Footer';
+import Navbar from '../Navbar';
 
 export default {
     components: {
-      Navbar,
-      Footer
+      Footer,
+      Navbar
     }
 }
 </script>
 
 <style scoped>
-
+.card {
+    box-shadow: 10px 10px grey;
+    background-color: #bfbfbf;
+}
 </style>
