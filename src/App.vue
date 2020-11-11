865b589278f580a2e80232fa0606692814ef42a8
hkm007 <hkm0072001@gmail.com>
2020-11-11 16:55:49 +0530

    project init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Navbar></Navbar>
+</template>
+
+<script>
+import Navbar from './components/Navbar';
+
+export default {
+  data() {
+    return {
+      title: "Pingoo"
+    }
+  },
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
